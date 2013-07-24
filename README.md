e668f1b1573713c0840148234f7f547d5ae4a640
ForbesLindesay <forbes@lindesay.co.uk>
2013-07-24 04:08:15 +0100

    Initial Code

---
+++ README.md
@@ -0,0 +1,120 @@
+# pull-request
+
+All the tools you need to commit to GitHub repos via the API and create pull requests
+
+Supports:
+
+ - exists: check if a repository already exists so you don't try and fork again
+ - fork: fork a repository so you get a copy to edit
+ - branch: create a new branch in a repository to edit
+ - commit: commit changes to files within a branch
+ - pull: create a pull request to have a human merge those changes
+
+[![Build Status](https://travis-ci.org/ForbesLindesay/pull-request.png?branch=master)](https://travis-ci.org/ForbesLindesay/pull-request)
+[![Dependency Status](https://gemnasium.com/ForbesLindesay/pull-request.png)](https://gemnasium.com/ForbesLindesay/pull-request)
+[![NPM version](https://badge.fury.io/js/pull-request.png)](http://badge.fury.io/js/pull-request)
+
+## Installation
+
+    npm install pull-request
+
+## API
+
+### pr.exists(user, repo, options, callback)
+
+Returns `true` if `github.com/:user/:repo` exists, and `false` if requesting that url returns an error.
+
+### pr.fork(from, to, repo, options, callback)
+
+Forks the repo `github.com/:from/:repo` to `github.com/:to/:repo` and waits until the fork operation completes.
+
+**options:**
+
+See [github-basic](https://github.com/ForbesLindesay/github-basic#options) and note that `auth` is **required**
+
+### pr.branch(user, repo, from, to, options, callback)
+
+Creates a new branch in `github.com/:user/:repo` using `from` as the source branch and `to` as the new branch name.
+
+### pr.commit(user, repo, commit, options, callback)
+
+Commits a set of changes to `github.com/:user/:repo`.  It only supports updating text files.
+
+**commit:**
+
+An object with:
+
+property | type                | default      | description
+---------|---------------------|--------------|----------------------------
+branch   | `String`            | `'master'`   | The branch to commit to
+message  | `String`            | **required** | The commit message
+updates  | `Array<FileUpdate>` | **required** | The actual changes to make
+
+**FileUpdate:**
+
+An object with:
+
+property | type     | default      | description
+---------|----------|--------------|----------------------------
+path     | `String` | **required** | The file path within the repo (e.g. `test/index.js`)
+content  | `String` | **required** | The new content of the file
+mode     | `String` | `'100644'`   | The mode to commit the file with (you probably don't want to change this)
+type     | `String` | `'blob'`     | The type of entry to create (you probably don't want to change this)
+
+**options:**
+
+See [github-basic](https://github.com/ForbesLindesay/github-basic#options) and note that `auth` is **required**
+
+Additionally the `force` object defaults to `false` and will force push the change if set to `true`.  You almost certainly don't want to do this.
+
+
+### pr.pull(from, to, message, options, callback)
+
+Creates a pull request from `from` to `to`.
+
+**from:**
+
+An object with:
+
+property | type     | default      | description
+---------|----------|--------------|----------------------------
+user     | `String` | **required** | The source user
+repo     | `String` | **required** | The source repository
+branch   | `String` | `'master'`   | The source branch
+
+**to:**
+
+An object with:
+
+property | type     | default      | description
+---------|----------|--------------|----------------------------
+user     | `String` | **required** | The destination user
+repo     | `String` | **required** | The destination repository
+branch   | `String` | `'master'`   | The destination branch
+
+**message:**
+
+Either:
+
+property | type     | default      | description
+---------|----------|--------------|----------------------------
+title    | `String` | **required** | The title of the pull request
+body     | `String` | `''`         | The body of the pull request
+
+or:
+
+property | type     | default      | description
+---------|----------|--------------|----------------------------
+issue    | `Number` | **required** | An issue number to convert into a pull request
+
+**options:**
+
+See [github-basic](https://github.com/ForbesLindesay/github-basic#options) and note that `auth` is **required**
+
+## Promises
+
+All APIs return promises if `callback` is ommitted.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,122 @@
+'use strict'
+
+var assert = require('assert')
+var ms = require('ms')
+var Promise = require('promise')
+var github = require('github').json
+
+function delay(time, value) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(value)
+    }, ms(time.toString()))
+  })
+}
+function poll(condition, options) {
+  options = options || {}
+  return condition().then(function (done) {
+    if (done) {
+      return done
+    } else if (options.timeout && options.start && Date.now() - options.start > ms(options.timeout.toString())) {
+      var err = new Error('operation timed out')
+      err.code = 'TIMEOUT'
+      throw err
+    } else {
+      return delay(typeof options.delay === 'function' ? options.delay(options.attempt || 1) : options.delay || 0).then(function () {
+        return poll(condition, {
+          attempt: (options.attempt || 1) + 1
+          delay: options.delay,
+          timeout: options.timeout,
+          start: options.start || Date.now()
+        })
+      })
+    }
+  })
+}
+
+exports.exists = exists
+function exists(user, repo, options, callback) {
+  options = JSON.stringify(JSON.parse(options || {}))
+  options.host = 'github.com'
+  return github('head', '/:user/:repo', {user: user, repo: repo}, options)
+    .then(function () {
+      return true
+    }, function () {
+      return false
+    }).nodeify(callback)
+}
+
+exports.fork = fork
+function fork(from, to, repo, options, callback) {
+  options = options || {}
+  return github('post', '/repos/:owner/:repo/forks', {owner: from, repo: repo, organization: to}, options)
+    .then(function (res) {
+      return poll(function () {
+        return exists(to, repo, options)
+      }, {timeout: options.timeout || '5 minutes', delay: function (attempt) { return (attempt * 5) + 'seconds' })
+      .then(function () { return res })
+    }).nodeify(callback)
+}
+
+exports.branch = branch
+function branch(user, repo, from, to, options, callback) {
+  github('get', '/repos/:owner/:repo/git/refs/:ref', {owner: user, repo: repo, ref: 'heads/' + from})
+    .then(function (res) {
+      return github('post', '/repos/:owner/:repo/git/refs', {owner: user, repo: repo, ref: 'refs/heads/' + to, sha: res.object.sha})
+    })
+}
+
+exports.commit = commit //does 5 API requests
+function commit(user, repo, commit, options, callback) {
+  var branch = commit.branch || 'master'
+  var message = commit.message
+  var updates = commit.updates
+  var shaLatestCommit, shaBaseTree, shaNewTree, shaNewCommit
+
+  Promise.from(null).then(function () {
+
+    //check for correct input
+    assert(user && typeof user === 'string', '`user` must be a string')
+    assert(repo && typeof repo === 'string', '`repo` must be a string')
+    assert(branch && typeof branch === 'string', '`branch` must be a string')
+    assert(message && typeof message === 'strin'g, '`message` must be a string')
+    updates.forEach(function (file) { // {path: path, content: content}
+      file.path = file.path.replace(/\\/g, '/').replace(/^\//, '')
+      file.mode = file.mode || '100644'
+      file.type = file.type || 'blob'
+      file.content = file.content
+    })
+
+
+    return github('get', '/repos/:owner/:repo/git/refs/:ref', {owner: user, repo: repo, ref: 'heads/' + branch}, options)
+  }).then(function (res) {
+    shaLatestCommit = res.object.sha
+    return github('get', '/repos/:owner/:repo/git/commits/:sha', {owner: user, repo: repo, sha: shaLatestCommit}, options)
+  }).then(function (res) {
+    shaBaseTree = res.tree.sha
+    return github('post', '/repos/:owner/:repo/git/trees', {owner: user, repo: repo, tree: updates, base_tree: shaBaseTree}, options)
+  }).then(function (res) {
+    shaNewTree = res.sha
+    return github('post', '/repos/:owner/:repo/git/commits', {owner: user, repo: repo, message: message, tree: shaNewTree, parents: [shaLatestCommit]}, options)
+  }).then(function (res) {
+    shaNewCommit = res.sha
+    return github('patch', '/repos/:owner/:repo/git/refs/:ref', {owner: user, repo: repo, ref: 'heads/' + branch, sha: shaNewCommit, force: options.force || false})
+  }).nodeify(callback)
+}
+
+exports.pull = pull
+function pull(from, to, msg, options, callback) {
+  var query = {
+    owner: to.owner,
+    repo: to.repo || from.repo,
+    base: to.branch || 'master',
+    head: from.user + ':' + (from.branch || 'master')
+  }
+  if (typeof msg.issue === 'number') {
+    query.issue = msg.issue.toString()
+  } else {
+    query.title = msg.title
+    query.body = msg.body || ''
+  }
+  return github('post', '/repos/:owner/:repo/pulls', query, options)
+}
